689c245f57eaecd964d9a64b110439e768156d09
Wuyialex <cci45@naver.com>
2017-02-04 17:29:41 +0900

    sd

---
+++ src/components/NewtimeComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//Newtime.css');
+
+class NewtimeComponent extends React.Component {
+  render() {
+    return (
+      <div className="newtime-component">
+        Please edit src/components///NewtimeComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+NewtimeComponent.displayName = 'NewtimeComponent';
+
+// Uncomment properties you need
+// NewtimeComponent.propTypes = {};
+// NewtimeComponent.defaultProps = {};
+
+export default NewtimeComponent;
