f69b6a2424ba2c70029bebfbbbfbf83d378c3536
Wuyialex <cci45@naver.com>
2017-02-04 17:36:18 +0900

    dfdfd

--- src/components/NewtimeComponent.js
+++ src/components/NewtimeComponent.js
@@ -1,17 +1,58 @@
 'use strict';
 
 import React from 'react';
-
+import axios from 'axios'
+import temp from '../tempdata.js'
 require('styles//Newtime.css');
 
 class NewtimeComponent extends React.Component {
-  render() {
-    return (
-      <div className="newtime-component">
-        Please edit src/components///NewtimeComponent.js to update this component!
-      </div>
-    );
-  }
+  constructor(props) {
+		super(props);
+		this.state = {
+			update: false
+		}
+		
+	}
+
+	getData(){
+
+	}
+
+	goTobroadCast(){
+
+	}
+
+
+	Allarticle(){
+	  var result =[]
+	  var func = function(message){	
+	  	temp.forEach(function(mes, index){
+	  		var one = <article key={index}>
+	  					<h1 title=""><span>{mes.date[0]}</span><span>{mes.date[1]}</span><span>{mes.date[2]}</span></h1>
+	  				    <div className='content'><p>{mes.title}</p><p>{mes.contents}</p></div>
+	  				    <div className="video-player">
+    					  <div className="embed-responsive embed-responsive-16by9 youtube">
+							<iframe width="560" height="315" src={mes.youtube}></iframe>
+							</div>
+    					</div>
+	  				   </article>
+	  		result.push(one)
+	  	})
+	  }
+	  func(message)
+	  return result
+	}
+    
+  
+	render() {
+		return (
+
+				<div className"App">
+				{this.Allarticle()}
+				</div> 
+		)
+	}
+
 }
 
 NewtimeComponent.displayName = 'NewtimeComponent';
---
+++ src/tempdata.js
@@ -0,0 +1,21 @@
+
+
+var timedata = [ {date: ['02', 'May', '2017'], 
+	title: 'timeline1', 
+	contents: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque vel natus inventore aperiam dicta quasi autem dolor! Nisi omnis maiores ut fugiat adipisci nesciunt hic non dolorem deleniti quasi.',
+	youtube: 'https://www.youtube.com/embed/7E0fVfectDo'
+}, {date: ['03', 'June', '2016'], 
+	title: 'timeline2', 
+	contents: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque vel natus inventore aperiam dicta quasi autem dolor! Nisi omnis maiores ut fugiat adipisci nesciunt hic non dolorem deleniti quasi.',
+	youtube:'https://www.youtube.com/embed/HwINhUczIlY'
+}, {date: ['04', 'May', '2015'], 
+	title: 'timeline3', 
+	contents: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque vel natus inventore aperiam dicta quasi autem dolor! Nisi omnis maiores ut fugiat adipisci nesciunt hic non dolorem deleniti quasi.',
+	youtube:'https://www.youtube.com/embed/cfOa1a8hYP8'
+}]
+
+var user = [ {username: 'svcoding', password: '1111'} ]
+
+
+export {timedata}
+
